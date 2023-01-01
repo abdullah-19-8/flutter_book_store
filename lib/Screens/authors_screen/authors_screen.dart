5de0560b956648c6a211c24ff3526a47a39ae12a
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2023-01-01 18:34:36 +0300

    authors screen done

--- lib/Screens/authors_screen/authors_screen.dart
+++ lib/Screens/authors_screen/authors_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../data/book_data.dart';
 
 class AuthorsScreen extends StatefulWidget {
   const AuthorsScreen({super.key});
@@ -9,8 +10,67 @@ class AuthorsScreen extends StatefulWidget {
 }
 
 class _AuthorsScreenState extends State<AuthorsScreen> {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Beginning app development with Flutter',
+      authorName: 'Rap Payne',
+      books: 3,
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Flutter Complete Reference',
+      authorName: 'Alberto Miola',
+      books: 2,
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Flutter in Action',
+      authorName: 'Kasper Lund',
+      books: 1,
+    ),
+    Transaction(
+      id: 't10',
+      title: 'Flutter y Firebase',
+      authorName: 'Rafael Barboza',
+      books: 1,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Authors'),
+      ),
+      body: ListView(
+        children: transactions.map((tx) {
+          return Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.authorName!,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${tx.books} books',
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      ),
+    );
   }
 }
--- lib/data/book_data.dart
+++ lib/data/book_data.dart
@@ -2,10 +2,12 @@ class Transaction {
   final String? id;
   final String? title;
   final String? authorName;
+  final int? books;
 
   Transaction({
     this.id,
     this.title,
     this.authorName,
+    this.books,
   });
 }
